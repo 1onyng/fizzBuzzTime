65617eda4dca91c51bd8b2f8d4a82c5871573d0f
tonyng <tonyng987@gmail.com>
2020-07-25 19:55:45 -0700

    Refactor script.js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-var startTimerButton = document.querySelector('.startTimer');
-var pauseTimerButton = document.querySelector('.pauseTimer');
-var timerDisplay = document.querySelector('.timer');
-var fizzBuzzDisplay = document.querySelector('.fizzBuzz');
-var startTime;
-var updatedTime;
-var difference;
-var tInterval;
-var savedTime;
-var paused = false;
-var running = false;
+let timerDisplay = document.querySelector('.timer'); 
+let fizzBuzzDisplay = document.querySelector('.fizzBuzz'); 
+let stopped = false; 
+let running = false;
+let startTime;
+let updatedTime;
+let diffTime;
+let tInterval;
+let savedTime; 
 
 // function changePage() {
-//   if (window.location.href === "https://1onyng.github.io/fizzBuzzTime/index.html") {
-//     sessionStorage.setItem("fizz", document.getElementById("fizz").value);
-//     sessionStorage.setItem("buzz", document.getElementById("buzz").value);
-//     window.location.href = "timer.html";
+//   if (window.location.pathname === "/https://1onyng.github.io/fizzBuzzTime/index.html") {
+//     let fizz = document.getElementById("fizz").value;
+//     let buzz = document.getElementById("buzz").value;
+//     if (isNaN(fizz) || isNaN(buzz)) {
+//       alert("Fizz and Buzz need to be a number. Please try again.");
+//     } else if (fizz < 2 || fizz > 10 || buzz < 2 || buzz > 10) {
+//       alert("Fizz and Buzz need to be within the prescribed range. Please try again.")
+//     } else {
+//       sessionStorage.setItem("fizz", fizz);
+//       sessionStorage.setItem("buzz", buzz);
+//       window.location.href = "timer.html";
+//     }
 //   } else {
 //     window.location.href = "index.html"
 //   }
@@ -22,8 +28,8 @@ var running = false;
 
 function changePage() {
   if (window.location.pathname === "/Users/tonyng/Desktop/fizzbuzztime/index.html") {
-    var fizz = document.getElementById("fizz").value;
-    var buzz = document.getElementById("buzz").value;
+    let fizz = document.getElementById("fizz").value;
+    let buzz = document.getElementById("buzz").value;
     if (isNaN(fizz) || isNaN(buzz)) {
       alert("Fizz and Buzz need to be a number. Please try again.");
     } else if (fizz < 2 || fizz > 10 || buzz < 2 || buzz > 10) {
@@ -42,8 +48,8 @@ function fizzBuzz(hr, min, sec) {
   let minSec = min * 60;
   let hrSec = hr * 60 * 60;
   let totalSec = minSec + hrSec + sec;
-  var fizz = sessionStorage.getItem("fizz");
-  var buzz = sessionStorage.getItem("buzz");
+  let fizz = sessionStorage.getItem("fizz");
+  let buzz = sessionStorage.getItem("buzz");
 
   if (totalSec % fizz == 0 && totalSec % buzz == 0) {
     fizzBuzzDisplay.innerHTML = "fizzbuzz";
@@ -59,18 +65,17 @@ function fizzBuzz(hr, min, sec) {
 function startTimer() {
   if (!running) {
     startTime = new Date().getTime();
-    tInterval = setInterval(getShowTime, 1000);
-    paused = false;
+    tInterval = setInterval(showTime, 1000);
+    stopped = false;
     running = true;
   }
 }
-function pauseTimer() {
-  if (!difference) {
-    // if timer never started, don't allow pause button to do anything
-  } else if (!paused) {
+
+function stopTimer() {
+  if (!stopped) {
     clearInterval(tInterval);
-    savedTime = difference;
-    paused = true;
+    savedTime = diffTime;
+    stopped = true;
     running = false;
   } else {
     resetTimer();
@@ -80,23 +85,25 @@ function pauseTimer() {
 function resetTimer() {
   clearInterval(tInterval);
   savedTime = 0;
-  difference = 0;
-  paused = false;
+  diffTime = 0;
+  stopped = false;
   running = false;
   timerDisplay.innerHTML = '0:00:00';
   fizzBuzzDisplay.innerHTML = '';
 }
 
-function getShowTime() {
+function showTime() {
   updatedTime = new Date().getTime();
+
   if (savedTime) {
-    difference = (updatedTime - startTime) + savedTime;
+    diffTime = (updatedTime - startTime) + savedTime;
   } else {
-    difference = updatedTime - startTime;
+    diffTime = updatedTime - startTime;
   }
-  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  let hours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diffTime % (1000 * 60)) / 1000);
 
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
@@ -104,8 +111,7 @@ function getShowTime() {
   if (hours === 9 && minutes === 59 && seconds === 59) {
     resetTimer();
     alert("Timer has expired.");
-  }
-  else {
+  } else {
     timerDisplay.innerHTML = hours + ':' + minutes + ':' + seconds;
     fizzBuzz(hours, minutes, seconds);
   }
